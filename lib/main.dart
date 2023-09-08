7ca6220166bc4f32af50f9e58e1962ad1f7bd411
Omkar-Shingte-Mobantica <134912748+Omkar-Shingte-Mobantica@users.noreply.github.com>
2023-09-08 13:55:31 +0530

    Api call sample code and integration to ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'models/Data.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -25,22 +28,69 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DataListItem extends StatefulWidget{
+class DataListItem extends StatefulWidget {
+  const DataListItem({super.key});
+
   @override
   State<DataListItem> createState() => _DataListItemState();
 }
 
 class _DataListItemState extends State<DataListItem> {
+  Future<ResponseClass>? dataArray;
+
+  Future<ResponseClass> fetchData() async {
+    final response =
+        await http.get(Uri.parse("https://randomuser.me/api/?results=20"));
+    if (response.statusCode == 200) {
+      return ResponseClass.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+  @override
+  void initState() {
+    // fetchData();
+    dataArray = fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      color: Colors.white,
+      child: FutureBuilder<ResponseClass>(
+        future: dataArray,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.results == null
+                    ? 0
+                    : snapshot.data!.results!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.cyan,
+                    ),
+                    title: Text(snapshot.data!.results![index].name!.title!),
+                    subtitle: Text(snapshot.data!.results![index].name!.first!),
+                    isThreeLine: true,
+                    trailing: Text(snapshot.data!.results![index].name!.last!),
+                  );
+                });
+          } else {
+            print("snapshot.error   ${snapshot.error} ");
+            return Text('${snapshot.error}');
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
 
 class DataList extends StatelessWidget {
+  const DataList({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/models/Data.dart
+++ lib/models/Data.dart
@@ -1,121 +1,364 @@
-class Data {
-  int? id;
-  String? name;
-  String? username;
+class ResponseClass {
+  List<Results>? results;
+  Info? info;
+
+  ResponseClass({this.results, this.info});
+
+  ResponseClass.fromJson(Map<String, dynamic> json) {
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
+    }
+    info = json['info'] != null ? Info.fromJson(json['info']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    if (info != null) {
+      data['info'] = info!.toJson();
+    }
+    return data;
+  }
+}
+
+class Results {
+  String? gender;
+  Name? name;
+  Location? location;
   String? email;
-  Address? address;
+  Login? login;
+  Dob? dob;
+  Dob? registered;
   String? phone;
-  String? website;
-  Company? company;
+  String? cell;
+  Id? id;
+  Picture? picture;
+  String? nat;
 
-  Data(
-      {this.id,
+  Results(
+      {this.gender,
       this.name,
-      this.username,
+      this.location,
       this.email,
-      this.address,
+      this.login,
+      this.dob,
+      this.registered,
       this.phone,
-      this.website,
-      this.company});
+      this.cell,
+      this.id,
+      this.picture,
+      this.nat});
 
-  Data.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    username = json['username'];
+  Results.fromJson(Map<String, dynamic> json) {
+    gender = json['gender'];
+    name = json['name'] != null ? Name.fromJson(json['name']) : null;
+    location = json['location'] != null
+        ? Location.fromJson(json['location'])
+        : null;
     email = json['email'];
-    address =
-        json['address'] != null ? new Address.fromJson(json['address']) : null;
+    login = json['login'] != null ? Login.fromJson(json['login']) : null;
+    dob = json['dob'] != null ? Dob.fromJson(json['dob']) : null;
+    registered = json['registered'] != null
+        ? Dob.fromJson(json['registered'])
+        : null;
     phone = json['phone'];
-    website = json['website'];
-    company =
-        json['company'] != null ? new Company.fromJson(json['company']) : null;
+    cell = json['cell'];
+    id = json['id'] != null ? Id.fromJson(json['id']) : null;
+    picture =
+        json['picture'] != null ? Picture.fromJson(json['picture']) : null;
+    nat = json['nat'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['username'] = this.username;
-    data['email'] = this.email;
-    if (this.address != null) {
-      data['address'] = this.address!.toJson();
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['gender'] = gender;
+    if (name != null) {
+      data['name'] = name!.toJson();
     }
-    data['phone'] = this.phone;
-    data['website'] = this.website;
-    if (this.company != null) {
-      data['company'] = this.company!.toJson();
+    if (location != null) {
+      data['location'] = location!.toJson();
     }
+    data['email'] = email;
+    if (login != null) {
+      data['login'] = login!.toJson();
+    }
+    if (dob != null) {
+      data['dob'] = dob!.toJson();
+    }
+    if (registered != null) {
+      data['registered'] = registered!.toJson();
+    }
+    data['phone'] = phone;
+    data['cell'] = cell;
+    if (id != null) {
+      data['id'] = id!.toJson();
+    }
+    if (picture != null) {
+      data['picture'] = picture!.toJson();
+    }
+    data['nat'] = nat;
     return data;
   }
 }
 
-class Address {
-  String? street;
-  String? suite;
+class Name {
+  String? title;
+  String? first;
+  String? last;
+
+  Name({this.title, this.first, this.last});
+
+  Name.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    first = json['first'];
+    last = json['last'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    data['first'] = first;
+    data['last'] = last;
+    return data;
+  }
+}
+
+class Location {
+  Street? street;
   String? city;
-  String? zipcode;
-  Geo? geo;
+  String? state;
+  String? country;
+  // int? postcode;
+  Coordinates? coordinates;
+  Timezone? timezone;
 
-  Address({this.street, this.suite, this.city, this.zipcode, this.geo});
+  Location(
+      {this.street,
+      this.city,
+      this.state,
+      this.country,
+      // this.postcode,
+      this.coordinates,
+      this.timezone});
 
-  Address.fromJson(Map<String, dynamic> json) {
-    street = json['street'];
-    suite = json['suite'];
+  Location.fromJson(Map<String, dynamic> json) {
+    street =
+        json['street'] != null ? Street.fromJson(json['street']) : null;
     city = json['city'];
-    zipcode = json['zipcode'];
-    geo = json['geo'] != null ? new Geo.fromJson(json['geo']) : null;
+    state = json['state'];
+    country = json['country'];
+    // postcode = json['postcode'];
+    coordinates = json['coordinates'] != null
+        ? Coordinates.fromJson(json['coordinates'])
+        : null;
+    timezone = json['timezone'] != null
+        ? Timezone.fromJson(json['timezone'])
+        : null;
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['street'] = this.street;
-    data['suite'] = this.suite;
-    data['city'] = this.city;
-    data['zipcode'] = this.zipcode;
-    if (this.geo != null) {
-      data['geo'] = this.geo!.toJson();
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (street != null) {
+      data['street'] = street!.toJson();
+    }
+    data['city'] = city;
+    data['state'] = state;
+    data['country'] = country;
+    // data['postcode'] = postcode;
+    if (coordinates != null) {
+      data['coordinates'] = coordinates!.toJson();
+    }
+    if (timezone != null) {
+      data['timezone'] = timezone!.toJson();
     }
     return data;
   }
 }
 
-class Geo {
-  String? lat;
-  String? lng;
+class Street {
+  int? number;
+  String? name;
 
-  Geo({this.lat, this.lng});
+  Street({this.number, this.name});
 
-  Geo.fromJson(Map<String, dynamic> json) {
-    lat = json['lat'];
-    lng = json['lng'];
+  Street.fromJson(Map<String, dynamic> json) {
+    number = json['number'];
+    name = json['name'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['lat'] = this.lat;
-    data['lng'] = this.lng;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['number'] = number;
+    data['name'] = name;
     return data;
   }
 }
 
-class Company {
-  String? name;
-  String? catchPhrase;
-  String? bs;
+class Coordinates {
+  String? latitude;
+  String? longitude;
 
-  Company({this.name, this.catchPhrase, this.bs});
+  Coordinates({this.latitude, this.longitude});
 
-  Company.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    catchPhrase = json['catchPhrase'];
-    bs = json['bs'];
+  Coordinates.fromJson(Map<String, dynamic> json) {
+    latitude = json['latitude'];
+    longitude = json['longitude'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['catchPhrase'] = this.catchPhrase;
-    data['bs'] = this.bs;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['latitude'] = latitude;
+    data['longitude'] = longitude;
+    return data;
+  }
+}
+
+class Timezone {
+  String? offset;
+  String? description;
+
+  Timezone({this.offset, this.description});
+
+  Timezone.fromJson(Map<String, dynamic> json) {
+    offset = json['offset'];
+    description = json['description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['offset'] = offset;
+    data['description'] = description;
+    return data;
+  }
+}
+
+class Login {
+  String? uuid;
+  String? username;
+  String? password;
+  String? salt;
+  String? md5;
+  String? sha1;
+  String? sha256;
+
+  Login(
+      {this.uuid,
+      this.username,
+      this.password,
+      this.salt,
+      this.md5,
+      this.sha1,
+      this.sha256});
+
+  Login.fromJson(Map<String, dynamic> json) {
+    uuid = json['uuid'];
+    username = json['username'];
+    password = json['password'];
+    salt = json['salt'];
+    md5 = json['md5'];
+    sha1 = json['sha1'];
+    sha256 = json['sha256'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['uuid'] = uuid;
+    data['username'] = username;
+    data['password'] = password;
+    data['salt'] = salt;
+    data['md5'] = md5;
+    data['sha1'] = sha1;
+    data['sha256'] = sha256;
+    return data;
+  }
+}
+
+class Dob {
+  String? date;
+  int? age;
+
+  Dob({this.date, this.age});
+
+  Dob.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    age = json['age'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['date'] = date;
+    data['age'] = age;
+    return data;
+  }
+}
+
+class Id {
+  String? name;
+  String? value;
+
+  Id({this.name, this.value});
+
+  Id.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    value = json['value'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['value'] = value;
+    return data;
+  }
+}
+
+class Picture {
+  String? large;
+  String? medium;
+  String? thumbnail;
+
+  Picture({this.large, this.medium, this.thumbnail});
+
+  Picture.fromJson(Map<String, dynamic> json) {
+    large = json['large'];
+    medium = json['medium'];
+    thumbnail = json['thumbnail'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['large'] = large;
+    data['medium'] = medium;
+    data['thumbnail'] = thumbnail;
+    return data;
+  }
+}
+
+class Info {
+  String? seed;
+  int? results;
+  int? page;
+  String? version;
+
+  Info({this.seed, this.results, this.page, this.version});
+
+  Info.fromJson(Map<String, dynamic> json) {
+    seed = json['seed'];
+    results = json['results'];
+    page = json['page'];
+    version = json['version'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['seed'] = seed;
+    data['results'] = results;
+    data['page'] = page;
+    data['version'] = version;
     return data;
   }
 }
