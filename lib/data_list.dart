9332180f423d0a59ae0ed120db618e9390be7bd5
OmkarShingte <shingteom@gmail.com>
2023-09-10 20:59:58 +0530

    Login ui

---
+++ lib/data_list.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'models/Data.dart';
+import 'package:http/http.dart' as http;
+
+class DataList extends StatelessWidget {
+  const DataList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.blue.shade400,
+          title: const Center(child: Text("Data List"))),
+      body: Container(
+        color: Colors.blue.shade50,
+        child: const DataListItem(),
+      ),
+    );
+  }
+}
+
+class DataListItem extends StatefulWidget {
+  const DataListItem({super.key});
+
+  @override
+  State<DataListItem> createState() => _DataListItemState();
+}
+
+class _DataListItemState extends State<DataListItem> {
+  Future<ResponseClass>? dataArray;
+
+  Future<ResponseClass> fetchData() async {
+    final response =
+        await http.get(Uri.parse("https://randomuser.me/api/?results=20"));
+    if (response.statusCode == 200) {
+      return ResponseClass.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // fetchData();
+    dataArray = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ResponseClass>(
+      future: dataArray,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.separated(
+            itemCount: snapshot.data!.results == null
+                ? 0
+                : snapshot.data!.results!.length,
+            itemBuilder: (context, index) {
+              var item = snapshot.data!.results![index];
+              var location =
+                  "${item.location!.city!}, ${item.location!.state!}, ${item.location!.country!}";
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.cyan,
+                          backgroundImage: NetworkImage(
+                              snapshot.data!.results![index].picture!.medium!),
+                        ),
+                      ),
+                      // ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "${item.name!.first!} ${item.name!.last!}",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 12.0),
+                                  child: Text(item.dob!.age!.toString()),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(location),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(item.email!.toString()),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          );
+        } else {
+          return Text('${snapshot.error}');
+        }
+        // return const CircularProgressIndicator();
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
 import 'package:first/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'Operations.dart';
+import 'data_list.dart';
 import 'models/Data.dart';
-import 'package:http/http.dart' as http;
 import 'network_call.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +22,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const DataList()
+        home: const Login()
+        // home: const DataList()
         // home: const NetworkCall()
         // home: const Operations(title: 'Flutter Home Page'),
         // home: const SplashScreen(),
@@ -32,124 +31,98 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DataListItem extends StatefulWidget {
-  const DataListItem({super.key});
-
-  @override
-  State<DataListItem> createState() => _DataListItemState();
-}
-
-class _DataListItemState extends State<DataListItem> {
-  Future<ResponseClass>? dataArray;
-
-  Future<ResponseClass> fetchData() async {
-    final response =
-        await http.get(Uri.parse("https://randomuser.me/api/?results=20"));
-    if (response.statusCode == 200) {
-      return ResponseClass.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load album');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    // fetchData();
-    dataArray = fetchData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<ResponseClass>(
-      future: dataArray,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.separated(
-            itemCount: snapshot.data!.results == null
-                ? 0
-                : snapshot.data!.results!.length,
-            itemBuilder: (context, index) {
-              var item = snapshot.data!.results![index];
-              var location =
-                  "${item.location!.city!}, ${item.location!.state!}, ${item.location!.country!}";
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: CircleAvatar(
-                          backgroundColor: Colors.cyan,
-                          backgroundImage: NetworkImage(
-                              snapshot.data!.results![index].picture!.medium!),
-                        ),
-                      ),
-                      // ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  "${item.name!.first!} ${item.name!.last!}",
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 12.0),
-                                  child: Text(item.dob!.age!.toString()),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(location),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(item.email!.toString()),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(),
-          );
-        } else {
-          return Text('${snapshot.error}');
-        }
-        // return const CircularProgressIndicator();
-      },
-    );
-  }
-}
-
-class DataList extends StatelessWidget {
-  const DataList({super.key});
+class Login extends StatelessWidget {
+  const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.blue.shade400,
-          title: const Center(child: Text("Data List"))),
-      body: Container(
-        color: Colors.blue.shade50,
-        child: const DataListItem(),
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.amberAccent,
+        title: const Text("Login Screen", style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 20.0),
+                child: SizedBox(
+                    child: Image.network(
+                        "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg")),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 4.0),
+                child: Text(
+                  "Mobile Number",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 6),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Enter mobile number",
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.zero)),
+                  keyboardType: TextInputType.number,
+                  maxLength: 10,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 6,
+                ),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      suffixIcon: Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          // add padding to adjust icon
+                          child: Icon(
+                            Icons.wifi_password,
+                            size: 24,
+                          )),
+                      suffixIconColor: Colors.blue,
+                      prefixIcon: Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          // add padding to adjust icon
+                          child: Icon(
+                            Icons.password,
+                            color: Colors.green,
+                            size: 24,
+                          )),
+                      hintText: "Enter Password",
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.zero)),
+                  keyboardType: TextInputType.number,
+                  maxLength: 10,
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(6),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green.shade500),
+                        // padding: MaterialStateProperty.all(EdgeInsets.all(50)),
+                        textStyle: MaterialStateProperty.all(const TextStyle(
+                            fontSize: 30, color: Colors.white))),
+                    child: const Text(
+                      "Login or Signup",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    )),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
