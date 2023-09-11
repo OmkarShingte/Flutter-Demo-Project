45bec160a90282926e81e3a5a39605f2d1455afd
Omkar-Shingte-Mobantica <134912748+Omkar-Shingte-Mobantica@users.noreply.github.com>
2023-09-11 12:48:35 +0530

    Minor code changes

---
+++ lib/counter_with_provider_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:first/counter_model.dart';
+
+class CounterWithProviderScreen extends StatefulWidget {
+  const CounterWithProviderScreen({super.key});
+
+  @override
+  State<CounterWithProviderScreen> createState() =>
+      _CounterWithProviderScreenState();
+}
+
+class _CounterWithProviderScreenState extends State<CounterWithProviderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CounterModel>(
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Counter with provider"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("You have pushed button this many times"),
+              Text(
+                "${value.count}",
+                style: Theme.of(context).textTheme.headlineMedium,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            final counter = context.read<CounterModel>();
+            counter.increment();
+          },
+          tooltip: "Increment",
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:first/splash_screen.dart';
 import 'package:first/login.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'counter_with_provider_screen.dart';
 import 'data_list.dart';
 import 'models/Data.dart';
 import 'network_call.dart';
@@ -34,48 +35,6 @@ class MyApp extends StatelessWidget {
       // home: const NetworkCall()
       // home: const Operations(title: 'Flutter Home Page'),
       // home: const SplashScreen(),
-    );
-  }
-}
-
-class CounterWithProviderScreen extends StatefulWidget {
-  const CounterWithProviderScreen({super.key});
-
-  @override
-  State<CounterWithProviderScreen> createState() =>
-      _CounterWithProviderScreenState();
-}
-
-class _CounterWithProviderScreenState extends State<CounterWithProviderScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<CounterModel>(
-      builder: (context, value, child) => Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("Counter with provider"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text("You have pushed button this many times"),
-              Text(
-                "${value.count}",
-                style: Theme.of(context).textTheme.headlineMedium,
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            final counter = context.read<CounterModel>();
-            counter.increment();
-          },
-          tooltip: "Increment",
-          child: const Icon(Icons.add),
-        ),
-      ),
     );
   }
 }
